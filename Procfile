d796b5f6887dad010a41382c790e74b0beb58109
red <tommyandrora@gmail.com>
2019-04-07 12:24:05 +0800

    modify the heroku bind port

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne chat.asgi_heroku:application --port $port --bind 0.0.0.0
+web: daphne chat.asgi_heroku:application --port $PORT --bind 0.0.0.0
 worker: python manage.py runworker channels -v2
--- chat/settings/heroku.py
+++ chat/settings/heroku.py
@@ -13,7 +13,7 @@ DATABASES = {
     }
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
